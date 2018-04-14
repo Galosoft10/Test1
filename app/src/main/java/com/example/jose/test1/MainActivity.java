30306192c0eb09477aa6e4d004ce2f1a33d17bca
Trinidad Garcia <josetrinidad@MacBook-Air-de-Trinidad.local>
2018-04-13 19:10:21 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jose/test1/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.jose.test1;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn, btnSignUp;
+    TextView txtSlogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        txtSlogan = findViewById(R.id.txtSlogan);
+
+        Typeface typeface = Typeface.createFromAsset(getAssets(),"fonts/london.ttf");
+        txtSlogan.setTypeface(typeface);
+
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/jose/test1/SignIt.java
@@ -0,0 +1,13 @@
+package com.example.jose.test1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignIt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_it);
+    }
+}
