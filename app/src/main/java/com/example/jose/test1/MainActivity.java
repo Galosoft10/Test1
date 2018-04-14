df8ae9434cb226412b298fbdaceb78f7344f10d2
josetrinidad <j_t_g_l@yahoo.com.mx>
2018-04-13 19:19:19 -0500

    Second commit

--- app/src/main/java/com/example/jose/test1/MainActivity.java
+++ app/src/main/java/com/example/jose/test1/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                Toast.makeText(MainActivity.this, "Hola", Toast.LENGTH_SHORT).show();
             }
         });
     }
